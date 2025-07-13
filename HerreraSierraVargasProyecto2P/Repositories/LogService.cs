97cf8d8f50a029bb79a4bc7165a87ebae807416f
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-07-13 18:43:41 -0500

    Creacion del LogService para el registro de operaciones en archivo de texto

--- HerreraSierraVargasProyecto2P/Repositories/LogService.cs
+++ HerreraSierraVargasProyecto2P/Repositories/LogService.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
 
 namespace HerreraSierraVargasProyecto2P.Repositories
 {
-    internal class LogService
+    public class LogService
     {
+        private readonly string _logFilePath;
+
+        public LogService(string fileName = "Logs_InventarioSistema.txt")
+        {
+            _logFilePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+        }
+
+        public async Task RegistrarAccionAsync(string mensaje)
+        {
+            string log = $"[{DateTime.Now:dd/MM/yyyy HH:mm}] {mensaje}\n";
+            await File.AppendAllTextAsync(_logFilePath, log);
+        }
+
+        public async Task<string[]> LeerLogsAsync()
+        {
+            if (!File.Exists(_logFilePath))
+                return new string[0];
+
+            return await File.ReadAllLinesAsync(_logFilePath);
+        }
+
+        public void BorrarLogs()
+        {
+            if (File.Exists(_logFilePath))
+                File.Delete(_logFilePath);
+        }
     }
 }
