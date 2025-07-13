c87266b2d335d1d8c83889bea70f8497679acc49
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-07-13 18:46:04 -0500

    Creacion del AppDataBase para la inicialización de SQLite y de los repositorios

--- HerreraSierraVargasProyecto2P/Repositories/AppDataBase.cs
+++ HerreraSierraVargasProyecto2P/Repositories/AppDataBase.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+using System.IO;
+using HerreraSierraVargasProyecto2P.Models;
 
 namespace HerreraSierraVargasProyecto2P.Repositories
 {
-    internal class AppDataBase
+    public class AppDatabase
     {
+        private static SQLiteAsyncConnection _database;
+
+        public GenericRepository<ProductoDto> ProductoRepo { get; }
+        public GenericRepository<ClienteDto> ClienteRepo { get; }
+        public GenericRepository<CategoriaDto> CategoriaRepo { get; }
+        public GenericRepository<PedidoDto> PedidoRepo { get; }
+        public GenericRepository<DetallePedidoDto> DetallePedidoRepo { get; }
+
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            ProductoRepo = new GenericRepository<ProductoDto>(_database);
+            ClienteRepo = new GenericRepository<ClienteDto>(_database);
+            CategoriaRepo = new GenericRepository<CategoriaDto>(_database);
+            PedidoRepo = new GenericRepository<PedidoDto>(_database);
+            DetallePedidoRepo = new GenericRepository<DetallePedidoDto>(_database);
+        }
     }
 }
