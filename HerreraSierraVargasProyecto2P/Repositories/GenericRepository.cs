fd013698ddee9a4f3fb62714bf196f7764459f22
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-07-13 18:35:18 -0500

    Creacion del GenericRepository para manejar los modelos en base génerico con SQLite

--- HerreraSierraVargasProyecto2P/Repositories/GenericRepository.cs
+++ HerreraSierraVargasProyecto2P/Repositories/GenericRepository.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using SQLite;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HerreraSierraVargasProyecto2P.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> where T : new()
     {
+        private readonly SQLiteAsyncConnection _database;
+
+        public GenericRepository(SQLiteAsyncConnection database)
+        {
+            _database = database;
+            _database.CreateTableAsync<T>().Wait();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return _database.Table<T>().ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            return _database.FindAsync<T>(id);
+        }
+
+        public Task<int> AddAsync(T item)
+        {
+            return _database.InsertAsync(item);
+        }
+
+        public Task<int> UpdateAsync(T item)
+        {
+            return _database.UpdateAsync(item);
+        }
+
+        public Task<int> DeleteAsync(T item)
+        {
+            return _database.DeleteAsync(item);
+        }
+
+        public Task<int> DeleteByIdAsync(int id)
+        {
+            return _database.DeleteAsync<T>(id);
+        }
     }
 }
